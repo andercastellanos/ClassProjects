bb1fcffacbb169941ee20346612f74083d13f54e
arturoescobar001 <145077671+arturoescobar001@users.noreply.github.com>
2023-09-15 21:40:10 -0400

    Update RoyalFlush.java

--- RoyalFlush.java
+++ RoyalFlush.java
@@ -1,21 +1,28 @@
+// Define a Java class named RoyalFlush that represents the "Royal Flush" poker hand category
 public class RoyalFlush extends HandCategory {
-    Suit suit;
-
+// Instance variable to store the suit of the Royal Flush
+Suit suit;
+    // Constructor that initializes the RoyalFlush object with a suit
     public RoyalFlush(Suit suit) {
         this.suit = suit;
     }
-
+   
+    // Override the toString method to provide a custom string representation of the RoyalFlush object
     @Override
     public String toString() {
         return "RoyalFlush [suit=" + suit + "]";
     }
-
+    
+    // Override the compareRank method to compare RoyalFlush objects based on the suit
     @Override
     int compareRank(Object right0) {
+        // Cast the right object to RoyalFlush
         RoyalFlush right = (RoyalFlush) right0;
+        // Compare the suits using the compareTo method of the Suit class
         return suit.compareTo(right.suit);
     }
 
+    // Override the category method to return the category code for Royal Flush
     @Override
     int category() {
         return 1;
