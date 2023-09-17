54245177ded55b6e8a81a87fb2309a95fbf2f32b
elyx26 <145077591+elyx26@users.noreply.github.com>
2023-09-16 20:31:01 -0400

    Update EmelyHandCategory.java

--- EmelyHandCategory.java
+++ EmelyHandCategory.java
@@ -1,14 +1,19 @@
 public abstract class HandCategory implements Comparable<HandCategory> {
+    // Abstract method for comparing the rank of two hands
     abstract int compareRank(Object right);
 
+    // Abstract method for determining the category of the hand
     abstract int category();
 
+    // Implementation of the compareTo method for comparing HandCategory objects
     public int compareTo(HandCategory right) {
+        // Compare the categories of two hand categories
         if (category() < right.category()) {
-            return 1;
+            return 1; // Return 1 if this hand's category is higher
         } else if (category() > right.category()) {
-            return -1;
+            return -1; // Return -1 if this hand's category is lower
         } else {
+            // If the categories are the same, delegate the comparison to compareRank
             return this.compareRank(right);
         }
     }
