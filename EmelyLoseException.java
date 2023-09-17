8242b419ad59bda0aeb32ee51a2c12a2d4000e2f
elyx26 <145077591+elyx26@users.noreply.github.com>
2023-09-16 20:52:52 -0400

    Update EmelyLoseException.java

--- EmelyLoseException.java
+++ EmelyLoseException.java
@@ -1,9 +1,10 @@
 public class LoseException extends Throwable {
-    String name;
-    Player player;
+    String name; // A string representing the name associated with the exception
+    Player player; // A reference to a Player object associated with the exception
 
+    // Constructor to create a LoseException with a name and a Player
     public LoseException(String name, Player player) {
-        this.name = name;
-        this.player = player;
+        this.name = name; // Assign the provided name to the name field
+        this.player = player; // Assign the provided Player object to the player field
     }
 }
