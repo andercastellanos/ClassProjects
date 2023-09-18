50e88e519f4730d33440e1f97ef92a9adb54af4c
andercastellanos <141978173+andercastellanos@users.noreply.github.com>
2023-09-18 13:09:19 -0400

    Update AndresFlush.java

--- AndresFlush.java
+++ AndresFlush.java
@@ -1,25 +1,26 @@
-public class Flush extends HandCategory {
-    Rank highest;
+public class Flush extends HandCategory { // Declare a class named "Flush" that inherits from another class named "HandCategory"
+    
+    Rank highest; // Declare a variable "highest" of type "Rank" to store the highest card in the flush
 
-    public Flush(Rank highest) {
-        this.highest = highest;
+    public Flush(Rank highest) { // Declare a constructor "Flush" that takes a "Rank" object as an argument and initializes AS "highest"
+        this.highest = highest; // Assign the parameter "highest" to the member variable "highest"
     }
 
-    @Override
+    @Override   // Override the "toString" method from the Object class to display the highest rank in the flush
     public String toString() {
-        return "Flush [highest=" + highest + "]";
-    }
+        return "Flush [highest=" + highest + "]"; // returns a string that starts with "Flush [highest=", followed by the highest rank contained in this Flush object.
+    } //This is the body of the method, and it specifies what string to return. 
 
-    @Override
-    int compareRank(Object right0) {
+    @Override 
+    int compareRank(Object right0) { // declares an int to take an object as an argument 
         Flush right = (Flush) right0;
-        return highest.compareTo(right.highest);
+        return highest.compareTo(right.highest); // line uses compareTo method of "RANK" of the "Flush" Object
     }
 
     @Override
     int category() {
         // TODO Auto-generated method stub
-        return 5;
+        return 5; // methods return an int (5) indicating the category type for a Flush hand
     }
 
 }
